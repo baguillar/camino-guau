985051147728be412229d62e3b4b3a66d384bf76
Beatriz Aguilar <beatrizaguilar@MacBook-Air.local>
2025-08-30 13:15:29 +0200

    prisma dogsex

---
+++ app/prisma/migrations/20250830111515_add_dog_attributes/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "public"."Dog" ADD COLUMN     "obedience" INTEGER,
+ADD COLUMN     "socializationWithDogs" INTEGER,
+ADD COLUMN     "socializationWithPeople" INTEGER,
+ADD COLUMN     "specialCharacteristic" TEXT;
